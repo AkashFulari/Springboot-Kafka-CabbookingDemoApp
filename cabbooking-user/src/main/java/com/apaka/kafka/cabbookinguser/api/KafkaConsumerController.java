2a6c80cf251802cbba3f948143be3727edd44e9b
Admin <akashfulari31@gmail.com>
2024-03-29 08:10:13 +0530

    New Updates for cab User

---
+++ cabbooking-user/src/main/java/com/apaka/kafka/cabbookinguser/api/KafkaConsumerController.java
@@ -0,0 +1,26 @@
+package com.apaka.kafka.cabbookinguser.api;
+
+import com.apaka.kafka.cabbookinguser.service.KafkaConsumerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Akash K. Fulari
+ * @Contact-mail: akashfulari31@gmail.com
+ * @Package: com.apaka.kafka.cabbookinguser.config
+ * @Description: cabbooking-driver - Project Development
+ * @Created: 2024/03/21 8:04 AM
+ **/
+@RestController
+@RequestMapping("kafka/api/v1/consumer")
+public class KafkaConsumerController {
+    @Autowired
+    KafkaConsumerService consumerService;
+
+    @GetMapping("receive")
+    public String recieveMessage(){
+        return consumerService.filterMessages();
+    }
+}
